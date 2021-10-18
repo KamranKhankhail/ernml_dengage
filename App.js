233d7083ca8293044f5392a386d056c7ba84731c
Kamran Younis <join.kamran92@gmail.com>
2021-10-18 16:12:50 +0500

    app setup to be working

--- App.js
+++ App.js
@@ -1,120 +1,254 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import * as React from 'react';
 
-import React, {useEffect} from 'react';
 import {
-  SafeAreaView,
+  Button,
+  Dimensions,
+  Platform,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TextInput,
   View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import Dengage from '@dengage-tech/react-native-dengage';
 
-const Section = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
+  const [contactKey, setContactKey] = React.useState('checking...');
+  const [subscription, setSubscription] = React.useState('');
+  const [subscriptionHidden, toggleSubscription] = React.useState(false);
 
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  const pageViewExample = () => {
+    // pageView event example
+    Dengage.pageView({
+      product_id: 1,
+      product_variant_id: 1,
+      quantity: 1,
+      unit_price: 10.0,
+      discounted_price: 9.99,
+    });
+  };
+  const addToCartExample = () => {
+    // addToCart action starts here.
+    // All items currently exists in shopping cart must be added to an array
+    const cartItem = {};
+    cartItem.product_id = 1;
+    cartItem.product_variant_id = 1;
+    cartItem.quantity = 1;
+    cartItem.unit_price = 10.0;
+    cartItem.discounted_price = 9.99;
+    // ... extra columns in shopping_cart_events_detail table, can be added in cartItem
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+    let cartItems = [];
+    cartItems.push(cartItem);
+    cartItems.push(cartItem);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    // Add to cart action
+    const addParams = {
+      product_id: 1,
+      product_variant_id: 1,
+      quantity: 1,
+      unit_price: 10.0,
+      discounted_price: 9.99,
+      // ... extra columns in shopping_cart_events table, can be added here
+      cartItems: cartItems, // all items in cart
+    };
+    Dengage.addToCart(addParams);
+    // addToCart action ends here.
+  };
+  const removeFromCartExample = () => {
+    const cartItem = {};
+    cartItem.product_id = 1;
+    cartItem.product_variant_id = 1;
+    cartItem.quantity = 1;
+    cartItem.unit_price = 10.0;
+    cartItem.discounted_price = 9.99;
+    // ... extra columns in shopping_cart_events_detail table, can be added in cartItem
+
+    let cartItems = [];
+    cartItems.push(cartItem);
+    cartItems.push(cartItem);
+
+    // Remove from cart action
+    const removeParams = {
+      product_id: 1,
+      product_variant_id: 1,
+      quantity: 1,
+      unit_price: 10.0,
+      discounted_price: 9.99,
+      // ... extra columns in shopping_cart_events table, can be added here
+      cartItems: cartItems, // all items in cart
+    };
+    Dengage.removeFromCart(removeParams);
+  };
+  const viewCart = () => {
+    const cartItem = {};
+    cartItem.product_id = 1;
+    cartItem.product_variant_id = 1;
+    cartItem.quantity = 1;
+    cartItem.unit_price = 10.0;
+    cartItem.discounted_price = 9.99;
+    // ... extra columns in shopping_cart_events_detail table, can be added in cartItem
+
+    let cartItems = [];
+    cartItems.push(cartItem);
+    cartItems.push(cartItem);
+    Dengage.viewCart(cartItems);
+  };
+  const appInboxExamples = async () => {
+    const inboxMessages = await Dengage.getInboxMessages(10, 20).catch(
+      err => err,
+    );
+    console.log('inboxMessages', inboxMessages);
+
+    const delResponse = await Dengage.deleteInboxMessage(
+      'your-message-id-here',
+    ).catch(res => res);
+    console.log('deleteInboxMessage: ', delResponse);
+
+    const messageSetAsInboxRes = await Dengage.setInboxMessageAsClicked(
+      'your-message-id-here',
+    ).catch(res => res);
+    console.log('messageSetAsInboxRes: ', messageSetAsInboxRes);
   };
 
-  useEffect(() => {
-    async function init() {
-      console.log(await Dengage.getToken());
-    }
+  React.useEffect(() => {
+    Dengage.setLogStatus(true);
     init();
   }, []);
 
+  const init = async () => {
+    if (Platform.OS === 'ios') {
+      Dengage.registerForRemoteNotifications(true);
+    }
+    setContactKey(await Dengage.getContactKey());
+    Dengage.setLogStatus(true);
+  };
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <ScrollView
+      contentContainerStyle={styles.contentContainerStyles}
+      style={styles.container}>
+      <View style={styles.btnContainer}>
+        <Button
+          onPress={async () => {
+            const hasPermissions = await Dengage.getUserPermission();
+            alert('hasPermissions: ' + hasPermissions);
+          }}
+          title={'check Permission'}
+        />
+      </View>
+
+      {Platform.OS === 'ios' && (
+        <View style={styles.btnContainer}>
+          <Button
+            onPress={() => {
+              Dengage.promptForPushNotificationsWitCallback(
+                async hasPermission => {
+                  console.log('hasPermission: ' + hasPermission);
+                  await Dengage.setUserPermission(Boolean(hasPermission));
+                },
+              );
+            }}
+            title={'ask Permission'}
+          />
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      )}
+
+      <View style={styles.btnContainer}>
+        <Button
+          onPress={async () => {
+            const token = await Dengage.getToken();
+            alert('token: ' + token);
+          }}
+          title={'get & show token'}
+        />
+      </View>
+
+      <View style={styles.btnContainer}>
+        <Button
+          onPress={async () => {
+            alert('contactKey: ' + (await Dengage.getContactKey()));
+          }}
+          title={'show Contact Key'}
+        />
+      </View>
+
+      <View style={styles.groupContainer}>
+        <Text>Edit/Enter Contact Key to update:</Text>
+        <TextInput
+          value={contactKey}
+          onChangeText={val => setContactKey(val)}
+          style={styles.input}
+        />
+
+        <View style={styles.btnContainer}>
+          <Button
+            onPress={async () => {
+              await Dengage.setContactKey(contactKey);
+            }}
+            title={'update Contact Key'}
+          />
+        </View>
+      </View>
+
+      <View style={styles.btnContainer}>
+        <Button
+          onPress={async () => {
+            if (!subscription) {
+              setSubscription(await Dengage.getSubscription());
+            } else {
+              toggleSubscription(!subscriptionHidden);
+            }
+          }}
+          title={'Show/Hide Current Subscription'}
+        />
+        {!subscriptionHidden && <Text>{`subscription: ${subscription}`}</Text>}
+      </View>
+
+      <View style={styles.btnContainer}>
+        <View style={styles.btnContainer}>
+          <Button
+            onPress={async () => {
+              const res = await Dengage.sendDeviceEvent('ky_device_event', {
+                test_event: 'this is test device event.',
+              });
+            }}
+            title={'send Device Event'}
+          />
+        </View>
+      </View>
+    </ScrollView>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {},
+  contentContainerStyles: {
+    width: Dimensions.get('window').width,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    padding: 20,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  heading: {
+    fontWeight: 'bold',
+    marginTop: 20,
   },
-  highlight: {
-    fontWeight: '700',
+  input: {
+    borderWidth: 1,
+    width: 250,
+    borderColor: 'black',
+  },
+  btnContainer: {
+    margin: 10,
+  },
+  groupContainer: {
+    borderWidth: 1,
+    borderColor: 'grey',
+    padding: 10,
   },
 });
-
-export default App;
--- android/app/src/main/java/com/testapp/MainActivity.java
+++ android/app/src/main/java/com/testapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.testapp;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
+import com.reactnativedengage.DengageRNCoordinator;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +14,19 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "testApp";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // These three lines need to be added
+    DengageRNCoordinator coordinator = DengageRNCoordinator.Companion.getSharedInstance();
+    coordinator.injectReactInstanceManager(getReactInstanceManager());
+    coordinator.setupDengage(
+            true,
+            "b84p3_p_l_wtgsvIHcPHnvTZIpX_s_l_Un7Iib2TLUBn6ILLxl_p_l_2ewbYdVKi7udHHp_p_l_RLypAdUu82jL67mLlrhZ27q7v2k0Lk1Dhc8HvpvwV9uC4zVb69bihjL_s_l_OyD69Aj_s_l_kZO9t",
+            null,
+            getApplicationContext()
+    );
   }
 }
